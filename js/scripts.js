74be32be07d4ae766826580c8ea536ed179d404c
J. TURNER <turner.j6@gmail.com>
2015-02-13 17:00:59 -0800

    Modify file.

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,7 @@ var triangleType = function(length) {
       || (length[b] >= (length[c] + length[a]))
       || (length[c] >= (length[a] + length[b]))) {
     triangle = "No luck ... that is not a triangle.";
-  } else {
-
-    if ((length[a] === length[b]) && (length[b] === length[c])) {
+  } else if ((length[a] === length[b]) && (length[b] === length[c])) {
       triangle = "equilateral";
     } else if (((length[a] === length[b]) && (length[a] !== length[c]))
         || ((length[b] === length[c]) && (length[b] !== length[a]))
@@ -20,9 +18,22 @@ var triangleType = function(length) {
     } else {
       triangle = "scalene";
     };
-  };
+
 
   result = triangle
   return result;
 
 };
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+
+    var result = triangle;
+
+
+    $(".triangle").text(triangle);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
