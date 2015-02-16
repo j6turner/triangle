1bd674b97826a1d420946744594e6876e0bf1626
J. TURNER <turner.j6@gmail.com>
2015-02-15 21:47:41 -0800

    Reconfigure first spec.

--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,9 @@
 describe('triangleType', function() {
 
   it('returns "No luck ... that is not a triangle."', function() {
-    expect(triangleType("[1], [1], [10]")).to.equal("No luck ... that is not a triangle.")
+    expect(triangleType(1, 1, 10)).to.equal("No luck ... that is not a triangle.")
   });
 
-  it('returns "equilateral" if all lengths are equal', function() {
-    expect(triangleType("[1], [1], [1]")).to.equal("equilateral");
-  });
 
-  it('returns "isosceles" if only two lengths are equal', function() {
-    expect(triangleType("[1], [1], [5]")).to.equal("isosceles");
-  });
-
-  it('returns "scalene" if no lengths are equal but no one length is greater than the other two combined', function() {
-    expect(triangleType("[2], [3], [4]")).to.equal("scalene");
-  });
 
 });
