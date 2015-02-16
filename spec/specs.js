37fc673b40dbe2f1a1dd81c20d99b19d81c9eac7
J. TURNER <turner.j6@gmail.com>
2015-02-15 21:53:14 -0800

    Reconfigure second spec.

--- spec/specs.js
+++ spec/specs.js
@@ -4,6 +4,9 @@ describe('triangleType', function() {
     expect(triangleType(1, 1, 10)).to.equal("No luck ... that is not a triangle.")
   });
 
+  it('returns "equilateral" if all lengths are equal', function() {
+    expect(triangleType(1, 1, 1)).to.equal("equilateral");
+  });
 
 
 });
