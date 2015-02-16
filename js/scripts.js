4af298f40967fdd569f633df3c8657da35e78c06
J. TURNER <turner.j6@gmail.com>
2015-02-15 21:51:13 -0800

    Refine code; pass first spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,12 @@
-var triangleType = function(length) {
+var triangleType = function(a, b, c) {
   var triangle = "";
-  var a = 0,
-      b = 0,
-      c = 0;
   var result = "";
 
-  if ((length[a] >= (length[b] + length[c]))
-      || (length[b] >= (length[c] + length[a]))
-      || (length[c] >= (length[a] + length[b]))) {
+  if (a >= (b + c)
+      || b >= (c + a)
+      || c >= (a + b)) {
     triangle = "No luck ... that is not a triangle.";
-  } else if ((length[a] === length[b]) && (length[b] === length[c])) {
-      triangle = "equilateral";
-  } else if (((length[a] === length[b]) && (length[a] !== length[c]))
-        || ((length[b] === length[c]) && (length[b] !== length[a]))
-        || ((length[c] === length[a]) && (length[c] !== length[b]))) {
-      triangle = "isosceles";
-  } else {
-      triangle = "scalene";
+
   };
 
 
@@ -24,16 +14,3 @@ var triangleType = function(length) {
   return result;
 
 };
-
-$(document).ready(function() {
-  $("form#triangle").submit(function(event) {
-
-    var result = triangle;
-
-
-    $(".triangle").text(triangle);
-
-    $("#result").show();
-    event.preventDefault();
-  });
-});
