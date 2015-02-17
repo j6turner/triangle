0681572b00851595aa65a36a4d4e6f4229870632
J. TURNER <turner.j6@gmail.com>
2015-02-17 14:23:51 -0800

    Refine code to list inputted dimensions according to triangle type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,52 @@
-var triangleType = function() {
-  var triangle = "";
-  var result = "";
+var triangle = {
+  a: 0,
+  b: 0,
+  c: 0,
+  scalene: "",
+  equilateral: "",
+  isosceles: "",
+
+   type: function() {
+     var a = this.a;
+     var b = this.b;
+     var c = this.c;
+    var notTriangle = (a >= (b + c)
+                        || b >= (c + a)
+                          || c >= (a + b)
+                            || isNaN(a)
+                              || isNaN(b)
+                                || isNaN(c));
+
+    if (notTriangle) {
+      result = null;
+    } else if (a === b && b === c) {
+        this.equilateral = "" + a + ", " + b + ", " + c;
+    } else if ((a === b && c < (a + b))
+          || (b === c && a < (b + c))
+          || (c === a && b < (a + b))) {
+        this.isosceles = "" + a + ", " + b + ", " + c;
+    } else {
+        this.scalene = "" + a + ", " + b + ", " + c;
+    };
 
 
-
-  if (a >= (b + c)
-      || b >= (c + a)
-      || c >= (a + b)
-      || isNaN(a)
-      || isNaN(b)
-      || isNaN(c)) {
-    triangle = null;
-  } else if (a === b && b === c) {
-      triangle = "equilateral";
-  } else if ((a === b && c < (a + b))
-        || (b === c && a < (b + c))
-        || (c === a && b < (a + b))) {
-      triangle = "isosceles";
-  } else {
-      triangle = "scalene";
-  };
-
-
-  result = triangle
-  return result;
-
-};
+  }
+}
 
 
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
-    var a = parseInt($("input#a").val());
-    var b = parseInt($("input#b").val());
-    var c = parseInt($("input#c").val());
-    var result = triangleType(a, b, c);
+    triangle.a = parseInt($("input#a").val());
+    triangle.b = parseInt($("input#b").val());
+    triangle.c = parseInt($("input#c").val());
+    var result = triangle.type();
 
     if (result === null) {
       alert("No luck ... that is not a triangle.")
     } else {
-      $(".a").text(a);
-      $(".b").text(b);
-      $(".c").text(c);
-      $(".equilateral").text(result);
-      $(".isosceles").text(result);
-      $(".scalene").text(result);
+      $(".equilateral").text(triangle.equilateral);
+      $(".isosceles").text(triangle.isosceles);
+      $(".scalene").text(triangle.scalene);
     }
 
     $("#equilateral").show();
